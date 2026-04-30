b103351dc0510ff5ab67c07329cee52d74a817cc
Jonassm <jonas-morbjerg@live.dk>
2026-04-30 11:32:21 +0200

    Progress bar, nuværende kunstner navn
    TODO: Tilføj artist ved ny tracks.csv fil.

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,24 @@ server.use(express.static("frontend"));
 server.use(onEachRequest);
 server.listen(port, onServerReady);
 
+//kendt kode fra dataforståelse
+server.get("/tracks", loadTracks); //dette giver os mulighed for at fetche noget fra /songs i frontend
+async function loadTracks(request, response) { //load songs til progress.js.
+  const dbResolve = await db.query(`
+    select tracks.length, tracks.title
+    from tracks
+  `);
+  const rows = dbResolve.rows;
+  if (rows.length === 0) {
+    response.sendStatus(404);
+  } else {
+    response.json(rows);
+  }
+}
+
+
 function onServerReady() {
-  console.log("Jukebox server running on port", port);
+  console.log("Populii server running on port", port);
 }
 
 function onEachRequest(request, response, next) {
---
+++ frontend/js/progress.js
@@ -0,0 +1,80 @@
+console.log("progress.js loaded") //debug, tjek lige at lortet loader
+
+async function loadTracks(title) { //loadtracks
+  const res = await fetch("/tracks"); //i server.js har vi lavet en /tracks ud fra SQL kald 
+  const data = await res.json();
+
+  tracks = data.map(track => ({ //her mapper vi tracks over til tracks index
+    title: track.title,
+    length: track.length
+  }));
+  playTrack(0);
+  console.log(tracks.length);
+}
+
+//god skik at definere alt med let, så det ikke bliver globalt
+let tracks = []; //vi starter med alt er nul, dvs. indexet er tomt, det starter ved nul.
+//obs: dette bliver jo redefineret ovenover, hvor vi laver data.map for tracks.
+let currentIndex = 0;
+let startTime = null;
+let length = 0;
+
+function playTrack(index) { //vi kører playTrack i indexet.
+  const elem = document.getElementById("timeBar");
+  elem.style.width = "0%"; //Vi starter fra ny - så vi skal reset timebar
+
+  const track = tracks[index];
+  if (!track) return; //hvis der ikke er en sang, kan vi ikke
+
+  length = track.length * 1000; //vores duration er givet i sekunder i .csv, så vi skal lige gange med 1000 da JS kører i millisekunder.
+  startTime = performance.now();
+
+  //Skriv den track & kunsterne der spiller lige nu (DOM)
+  const tracktitle = document.getElementById("tracktitle");
+  tracktitle.textContent = track.title
+  const artistname = document.getElementById("artistname");
+  artistname.textContent = ("placeholder")
+  //TODO ved ny DB skal vi grabbe artist direkte fra tracks.
+  
+  
+  /*
+performance.now er et kald der i javascript giver et præcist antal millisekunder siden kaldet.
+Hvis man brugte noget hvor mange brugte computeresn forståelse af tid kan der være problemer hvis der er performance forskelle mellem siden og computeren.
+  */
+  requestAnimationFrame(updateProgress); //opdatere via requestAnimationFrame (JS funktion)
+
+}
+
+function updateProgress(now) { //nu definere vi vores updateProgress. hvor "now" er det vi har fra performance.now
+  if (startTime == null) return; // præ kondition: hvis starttiden ikke er defineret kan vi ikke kører det
+
+  const elapsed = now - startTime; //tiden der er gået er nu - starttid.
+  const progress = Math.min(elapsed / length, 1);
+  /*
+  Vi bruger mathmin til at finde ud af, om elapsed / duration er over 1.
+  Hvis det skulle være over 1, forcer vi det til at være 1 (100%)
+  */
+  const elem = document.getElementById("timeBar"); //DOM på timeBar
+  elem.style.width = (progress * 100) + "%"; //opdatere timebar med progress. da progress er i decimal, skal vi gange med 100 for at få procent.
+
+  if (progress < 1) { //Hvis sangen ikke er færdig opdatere vi progress.
+    requestAnimationFrame(updateProgress); 
+    //console.log("progress:", progress); //debug, spammer konsol
+  } else {
+    console.log("next track runs") //debug
+    nextTrack(); //ellers starter vi næste sang. Funktionen defineres næste linjer.
+  }
+}
+
+function nextTrack() {
+  currentIndex++; //plus en på indekset, da vi har været en igennem
+
+  if (currentIndex < tracks.length) { //præ konditioner: tjek, at der stadig er sange i indexet
+    playTrack(currentIndex);
+  } else {
+    currentIndex = 0; //ellers starter vi forfra
+    playTrack(currentIndex);
+  }
+}
+
+loadTracks(); // det her er kickoffet
