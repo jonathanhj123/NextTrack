9eac7491c7eb984ea2c033809083428e29592301
Jonathan Jacobsen <jona246@gmail.com>
2026-04-30 12:15:52 +0200

    Added login constraints and login now checks if the user exists and if the password matches the user if it exists

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,11 @@ const db = pool();
 const port = 3010;
 const server = express();
 server.use(express.static("frontend"));
+server.use(express.json());
 server.use(onEachRequest);
 server.listen(port, onServerReady);
+server.get("/api/checkIfUserExists/:username", checkIfUserExists);
+server.post("/api/checkPassword", checkPassword);
 
 function onServerReady() {
   console.log("Jukebox server running on port", port);
@@ -17,4 +20,33 @@ function onServerReady() {
 function onEachRequest(request, response, next) {
   console.log(new Date(), request.method, request.url);
   next();
+}
+
+async function checkIfUserExists(request, response) {
+  const username = request.params.username;
+
+  const dbResult = await db.query(
+    `
+    Select Exists
+    (select username
+    from users
+    where username = $1)`,
+    [username],
+  );
+  response.json(dbResult.rows[0].exists);
+}
+
+async function checkPassword(request, response) {
+  try {
+    const { username, password } = request.body;
+    const dbResult = await db.query(
+      "SELECT password FROM users WHERE username = $1",
+      [username],
+    );
+    const match = dbResult.rows[0].password === password;
+    response.json({ match });
+  } catch (err) {
+    console.error(err);
+    response.status(500).json({ error: err.message });
+  }
 }
--- frontend/js/login.js
+++ frontend/js/login.js
@@ -3,7 +3,35 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   // your validation / fetch call here if needed
-  window.location.href = "session.html";
-  const username = document.querySelector(#username).value;
-  const username = document.querySelector(#password).value;
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+
+  checkLogin(username, password);
+
+  async function checkLogin(username, password) {
+    const response = await fetch(`/api/checkIfUserExists/${username}`);
+    console.log(response);
+    if (response.ok) {
+      const userExists = await response.json();
+      console.log(userExists);
+      if (userExists) {
+        if (await checkPassword(username, password)) {
+          window.location.href = "session.html";
+        }
+      }
+    }
+  }
+
+  console.log(username);
+  console.log(password);
 });
+
+async function checkPassword(username, password) {
+  const response = await fetch(`/api/checkPassword`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+  const data = await response.json();
+  return data.match;
+}
