f292d257a32ddbfd30ee63b88a2ed9ad8b784a8c
Jonathan Jacobsen <jona246@gmail.com>
2026-04-30 10:34:33 +0200

    xD

---
+++ frontend/js/login.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // your validation / fetch call here if needed
+  window.location.href = "session.html";
+  const username = document.querySelector(#username).value;
+  const username = document.querySelector(#password).value;
+});
