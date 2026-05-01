e3290876ff744d6d33e89f243adb82f1410c0551
Jakoarn <jakob@arnkjaer.de>
2026-05-01 11:06:29 +0200

    startet med vote.js

---
+++ frontend/js/vote.js
@@ -0,0 +1,31 @@
+/*
+Vote = 8 songs + 8 artists combined - title on top, artist below
+Arrow up on the right, grey when not selected, red when selected
+Should be selectable, should be deselectable, should make sure that a user can only upvote 1 song, the rest should not be selectable when user has used their upvote
+Should display count to the left of the button
+JS bool to check if user has upvoted once
+*/
+
+console.log("vote.js loaded succesfully!")
+
+
+const query = document.getElementById("leftBottomRightDiv");
+await buildSongQueue(query);
+
+const tracksQueue = [];
+
+async function buildSongQueue() {
+    const response = await fetch ("/tracks");
+    const rows = await response.json();
+
+    tracksQueue.length = 0;
+
+    for (let i = 0; i < 8; i++){
+        const row = rows[i];
+        tracksQueue.push({
+            title: rows[i].title,
+            artist: rows[i].artist,
+        });
+        return row;
+    }
+}
