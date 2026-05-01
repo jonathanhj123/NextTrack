22a4321fea6fbd8cae3ac23b864eb11d5ec44a24
Jonassm <jonas-morbjerg@live.dk>
2026-05-01 10:33:24 +0200

    Fuckup med git fiks

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,6 @@ function onEachRequest(request, response, next) {
   next();
 } //logging
 
-server.get("/api/checkIfUserExists/:username", checkIfUserExists);
-server.post("/api/checkPassword", checkPassword);
-
 //kendt kode fra dataforståelse
 server.get("/tracks", loadTracks); //dette giver os mulighed for at fetche noget fra /songs i frontend
 async function loadTracks(request, response) { //load songs til progress.js.
@@ -36,39 +33,6 @@ async function loadTracks(request, response) { //load songs til progress.js.
     response.json(rows);
   }
 }
-
-
-
-async function checkIfUserExists(request, response) {
-  const username = request.params.username;
-
-  const dbResult = await db.query(
-    `
-    Select Exists
-    (select username
-    from users
-    where username = $1)`,
-    [username],
-  );
-  response.json(dbResult.rows[0].exists);
-}
-
-async function checkPassword(request, response) {
-  try {
-    const { username, password } = request.body;
-    const dbResult = await db.query(
-      "SELECT password FROM users WHERE username = $1",
-      [username],
-    );
-    const match = dbResult.rows[0].password === password;
-    response.json({ match });
-  } catch (err) {
-    console.error(err);
-    response.status(500).json({ error: err.message });
-  }
-}
-
-
 
 function onServerReady() {
   console.log("Populii server running on port", port);
