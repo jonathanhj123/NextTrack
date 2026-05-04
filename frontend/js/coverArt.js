d334fd0620bf93286ee4403d9111f27240339034
Andreas de Feuth <andreasdefeuth@gmail.com>
2026-05-04 15:18:33 +0200

    Icon

--- frontend/js/coverArt.js
+++ frontend/js/coverArt.js
@@ -1,7 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
   const coverArtContainer = document.getElementById("coverArt");
-
-  if (coverArtContainer) {
     const img = document.createElement("img");
     img.src = "coverart.jpg";
     img.alt = "Cover Art";
@@ -12,7 +10,4 @@ window.addEventListener("DOMContentLoaded", () => {
     img.style.borderRadius = "5px";
 
     coverArtContainer.appendChild(img);
-  } else {
-    console.error("Could not find the coverArt container!");
-  }
 });
