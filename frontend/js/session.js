237017b5d36104a26ea85d1320199a8bc44e7d4f
Jonassm <jonas-morbjerg@live.dk>
2026-05-08 13:32:28 +0200

    Første stretch i votes

---
+++ frontend/js/session.js
@@ -0,0 +1,33 @@
+const joinbutton = document.getElementById("joinButton");
+joinbutton.addEventListener("click", () => {
+    joinRedirect();
+});
+
+
+async function joinRedirect() { 
+  try {
+
+    const params = new URLSearchParams(window.location.search); 
+    const user_id = params.get("user_id");
+      window.location.href = `/join.html?user_id=${user_id}`;
+
+  } catch (err) { 
+    showPopup("Session does not exist");
+    return;
+  }
+}
+
+//popup, samme som i join.js
+function showPopup(message) {
+  if (document.querySelector(".error-popup")) return;
+
+  const popup = document.createElement("div");
+  popup.className = "error-popup";
+  popup.textContent = message;
+  document.body.appendChild(popup);
+
+  setTimeout(() => {
+    popup.remove();
+  }, 2500);
+}
+
