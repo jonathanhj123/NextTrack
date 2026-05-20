ec60ff89cef03edc6fa777f86b75e32eb2e08a15
Jonassm <jonas-morbjerg@live.dk>
2026-05-20 10:29:43 +0200

    init

--- frontend/js/load.js
+++ frontend/js/load.js
@@ -7,7 +7,8 @@ async function loadSession() {
     const params = new URLSearchParams(window.location.search);
     const session_id = params.get("session"); //få session id ud af query parameteren... Samme kode som lige overfor ved leave button.
     const response = await fetch (`/api/getCurrentStatus?session_id=${session_id}`); //samme funktion som progress har. vi laver altså en ny tracks side for hver session _id. 
-    const rows = await response.json();
+    const rows = await response.JSON();
+    console.log(JSON);
     } catch(err) {
     console.log(err);
     }
